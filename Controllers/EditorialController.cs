64c2168cbba3e8a00abfe4f37ba522b8a191190c
Daniel Abad <dabad@iconmm.com>
2022-06-07 20:35:46 +0200

    Iteration: Ej.4.2

---
+++ Controllers/EditorialController.cs
@@ -0,0 +1,25 @@
+﻿using Ejercicio_Sesión_1.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ejercicio_Sesión_1.Controllers
+{
+    [ApiController]
+    [Route("api/EditorialController")]
+    public class EditorialController : ControllerBase
+    {
+
+        private readonly ApplicationDbContext context;
+
+        public EditorialController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        //4.2
+        [HttpGet("ListEditoriales")]
+        public async Task<IEnumerable<Editorial>> Get()
+        {
+            return await context.Editoriales.ToListAsync();
+        }
+    }
+}
