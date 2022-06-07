0495877c6a0d33fb8ca4a7cf520d41e2512238e7
Daniel Abad <dabad@iconmm.com>
2022-06-07 18:38:59 +0200

    Iteration: first commit

---
+++ WebApiBiblioteca/DTOs/DTOEditorialLibro.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApiBiblioteca.DTOs
+{
+    // 4.4.a
+    public class DTOEditorialLibro
+    {
+        public int IdEditorial { get; set; }
+        public string Nombre { get; set; }
+        public List<DTOLibroItem> Libros { get; set; }
+    }
+
+    public class DTOLibroItem
+    {
+        public int IdLibro { get; set; }
+        public string Nombre { get; set; }
+        public int Paginas { get; set; }
+    }
+}
