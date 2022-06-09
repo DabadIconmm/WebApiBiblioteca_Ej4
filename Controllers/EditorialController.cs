0121e9367210ab5d07711039ec0d754357c57d74
Daniel Abad <dabad@iconmm.com>
2022-06-09 18:38:02 +0200

    Iteration: Ej. 4.5 to 4.8

--- Controllers/EditorialController.cs
+++ Controllers/EditorialController.cs
@@ -51,7 +51,7 @@ namespace Ejercicio_Sesión_1.Controllers
             }
             return Ok(editorial);
         }
-        //4.4.a
+        //4.4.a DTOEditorialLibro
         [HttpGet("editorialeslibrosdto/{id:int}")]
         public async Task<ActionResult<Editorial>> GetEditorialesLibrosDTO(int id)
         {
@@ -73,6 +73,99 @@ namespace Ejercicio_Sesión_1.Controllers
                 return NotFound();
             }
             return Ok(editorial);
+        }
+        //4.5
+        [HttpPost]
+        public async Task<ActionResult> PostEditorial(Editorial editorial)
+        {
+            //estado de la familia.
+            var statusEditorial = context.Entry(editorial).State; // Detached, sin seguimiento (viene de fuera y no se conoce)
+            //Otra forma de hacer lo anterior
+            //context.Entry(editorial).State = EntityState.Added;
+            await context.AddAsync(editorial);
+            var statusEditoria2 = context.Entry(editorial).State; // Added, agregado y conocido
+            await context.SaveChangesAsync();
+            var statusEditorial3 = context.Entry(editorial).State; // Unchanged. Cambiado y sin modificar
+            return Ok();
+        }
+
+        //4.6
+        [HttpPost("editoriallibros")]
+        public async Task<ActionResult> PostDTOEditorialLibros(DTOEditorialLibro editorialLibros)
+        {
+            using var transaction = await context.Database.BeginTransactionAsync();
+            try
+            {
+                var newEditorial = new Editorial()
+                {
+                    Nombre = editorialLibros.Nombre
+                };
+
+                await context.AddAsync(newEditorial);
+                await context.SaveChangesAsync();
+
+                foreach (var libro in editorialLibros.Libros)
+                {
+                    var newLibro = new Libro()
+                    {
+                        Titulo = libro.Nombre,
+                        Paginas = libro.Paginas,
+                        Editorial = newEditorial
+                    };
+
+                    await context.AddAsync(newLibro);
+                }
+
+                await context.SaveChangesAsync();
+                await transaction.CommitAsync();
+
+                return Created("Editorial", new { editorial = newEditorial });
+            }
+            catch (Exception ex)
+            {
+                await transaction.RollbackAsync();
+                return BadRequest("Se ha producido un error" + ex.Message);
+            }
+        }
+
+        // 4.7
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> PutEditorial([FromRoute] int id, [FromBody] Editorial editorial)
+        {
+            if (id != editorial.Id)
+            {
+                return BadRequest("Los ids proporcionados son diferentes");
+            }
+            var existe = await context.Editoriales.AnyAsync(x => x.Id == id);
+            if (!existe)
+            {
+                return NotFound();
+            }
+            context.Update(editorial);
+
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        // 4.8
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var editorial = await context.Editoriales.Include(x => x.Libros).FirstOrDefaultAsync(x => x.Id == id);
+
+            if (editorial is null)
+            {
+                return NotFound();
+            }
+
+            if (editorial.Libros.Count > 0)
+            {
+                return BadRequest("Esta editorial contiene libros. No se puede eliminar");
+            }
+
+            context.Remove(editorial);
+            await context.SaveChangesAsync();
+            return Ok();
         }
 
     }
